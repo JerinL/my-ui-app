6ba09b25ac72f094de5aea9568a1e2cb0adbda82
Jerin <jerin.l@aryanow.com>
2024-09-04 14:56:38 +0530

    new changes

--- src/MyDashBoard/Dashboard.jsx
+++ src/MyDashBoard/Dashboard.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Dashboard.css";
+import profile_Image from "../Assets/assets/IMG_7963.jpg";
+import "@fontsource/roboto";
+import "@fontsource/open-sans";
+import "@fontsource/merriweather";
+import "@fontsource/source-code-pro";
+import "@fontsource/lora";
 
 const Dashboard = () => {
   function getBrowserDetails() {
     const dataArr = ["JERIN", "JACK"];
     const num = Math.floor(Math.random() * dataArr.length);
     return dataArr[num];
+  }
+
+  function getFontStyle() {
+    const fontStyle = ["italic"];
+    const num = Math.floor(Math.random() * fontStyle.length);
+    return fontStyle[num];
   }
 
   function giveAccess() {
@@ -21,25 +33,54 @@ const Dashboard = () => {
   }
   const year = new Date();
 
-  console.log(year.getDate())
+  console.log(year.getDate());
   return (
     <>
       <header className="header">
         <div className="profile">
-          <h1 className="profile-img">I</h1>
-        <h1>Hii {giveAccess()}</h1>
+          <img className="profile-img" src={profile_Image} />
+          <h1>Hii {giveAccess()}</h1>
         </div>
-        <nav >
+        <nav>
           <ul className="nav-items">
-            <li><a className="nav-items-item" href="home">HOME</a></li>
-            <li><a className="nav-items-item" href="skill">SKILS</a></li>
-            <li><a className="nav-items-item" href="skill">PROJECTS</a></li>
-            <li><a className="nav-items-item" href="skill">CONTACT</a></li>
+            <li>
+              <a className="nav-items-item" href="home">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a className="nav-items-item" href="skill">
+                SKILS
+              </a>
+            </li>
+            <li>
+              <a className="nav-items-item" href="skill">
+                PROJECTS
+              </a>
+            </li>
+            <li>
+              <a className="nav-items-item" href="skill">
+                CONTACT
+              </a>
+            </li>
           </ul>
-        </nav>
+        </nav>  
       </header>
       <main className="main">
-        <p>main datas are here</p>
+        <div className="main-content">
+          <h1>HEY, I'M JERIN</h1>
+          <p
+            style={{
+              fontSize: "20px",
+              textAlign: "center",
+              paddingTop: "30px"
+            }}
+          >
+            A Result-Oriented Web Developer building and managing Websites and
+            Web <br/>Applications that leads to the success of the overall product
+          </p>
+          <a className="project-button">PROJECTS</a>
+        </div>
       </main>
       <footer className="footer">
         <a>Copyright &copy; {year.getFullYear()}</a>
